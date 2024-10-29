a88b781783a421d8187291299ca673b9c956ef16
ROBERTSPHXINTERNET <robert.s@phxinternet.com>
2024-10-28 22:19:42 -0700

    Added logout functionality

--- app.py
+++ app.py
@@ -128,5 +128,10 @@ def login():
 
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.run(debug=True)
